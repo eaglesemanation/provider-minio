2d078283bc2cb8d0d41b2cdc82dd832b654bc6e1
Vladimir Romashchenko <eaglesemanation@gmail.com>
2026-01-02 20:53:05 -0500

    Split controller into cluster and namespaced

---
+++ internal/controller/namespaced/zz_setup.go
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: 2024 The Crossplane Authors <https://crossplane.io>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package controller
+
+import (
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/crossplane/upjet/v2/pkg/controller"
+
+	policy "github.com/eaglesemanation/provider-minio/internal/controller/iam/policy"
+	user "github.com/eaglesemanation/provider-minio/internal/controller/iam/user"
+	userpolicyattachment "github.com/eaglesemanation/provider-minio/internal/controller/iam/userpolicyattachment"
+	accesskey "github.com/eaglesemanation/provider-minio/internal/controller/minio/accesskey"
+	providerconfig "github.com/eaglesemanation/provider-minio/internal/controller/providerconfig"
+	bucket "github.com/eaglesemanation/provider-minio/internal/controller/s3/bucket"
+)
+
+// Setup creates all controllers with the supplied logger and adds them to
+// the supplied manager.
+func Setup(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		policy.Setup,
+		user.Setup,
+		userpolicyattachment.Setup,
+		accesskey.Setup,
+		providerconfig.Setup,
+		bucket.Setup,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
